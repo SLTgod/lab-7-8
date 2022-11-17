45539ca1ec38d20cde8ff7db68059d9a75e42bee
Kirill Korney <117628644+SLTgod@users.noreply.github.com>
2022-11-17 10:43:44 +0300

    Add files via upload

---
+++ сумма 2050.pas
@@ -0,0 +1,22 @@
+﻿program hislo;
+var t,n,s,o:int64;
+begin
+  readln(t);
+  for s:= 1 to t do
+    begin
+    readln(n);
+    if (n mod 2050)<>0 then
+      writeln(-1)
+    else
+    begin
+    n:=n div 2050;
+    o:=0;
+    while n<>0 do
+    begin
+    o:=o+(n mod 10);
+    n:=n div 10;
+    end;
+    writeln(o);
+    end;
+    end;
+end.
